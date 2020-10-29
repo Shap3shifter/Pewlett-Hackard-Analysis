373ad4936ca98eaedee7064c00f3b4bfec1e4818
Shap3shifter <69819693+Shap3shifter@users.noreply.github.com>
2020-10-28 20:34:52 -0700

    Adding queries.sql file along with retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -17,8 +17,8 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
